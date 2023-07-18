39a9ce134e38d2fb088acfd6c1d23ba3857a93df
christiannaira <christianmnaira@gmail.com>
2023-07-18 15:22:33 +0630

    designed the portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ function Portfolio() {
 
             <section className="portfolio" id="portfolio">
 
-                <div className="portfolio-content container-sm border">
+                <div className="portfolio-content container-sm">
 
                     <div className="portfolio-main">
                         <h1 className='d-flex align-items-center'>PROJECTS <img src={FastForward} alt="" width={'100px'} className='rounded-pill ms-3' /> </h1>
@@ -16,13 +16,19 @@ function Portfolio() {
                     <div className="portfolio-projects row">
 
                         <div className="project01 project col-md-12">
-                            asd
+                            <h2>ESTYLE</h2>
+                        </div>
+
+                        <div className="project02 project col-md-12">
+                            <h2>EHUB</h2>
+                        </div>
+
+                        <div className="project03 project col-md-12">
+                            <h2>FUELFLOW</h2>
                         </div>
 
 
-                        <div className="project01 project col-md-12">
-                            asd
-                        </div>
+
 
                     </div>
 
