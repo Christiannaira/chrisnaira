7e4ba4c34eb38fe89584b46a4ee60d082e3095b9
christiannaira <christianmnaira@gmail.com>
2023-07-25 14:13:33 +0630

    designed the project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
     return (
         <>
 
-            <section className="projects" id="projects">
+            <section className="projects border" id="projects">
                 <div className="projects-content container-fluid-sm">
 
                     <div className="projects-main">
@@ -17,16 +17,25 @@ function Projects() {
 
                     <div className="project-demos">
 
-                        <div className="project-demo row">
+                        <div className="project-demo ">
 
-                            <div className="project-img col-md-6 demoOne">
+                            <div className="project-img demoOne">
 
                                 <img src={Estyle} alt="" />
 
-                                <div className="layer">
+                                <div className="project-txt">
 
-                                    <div className="project-tools">
+                                    <h3>Estyle Web Design</h3>
 
+                                    <p>Estyle is an ecommerce platform where users who like fashions can purchase what they want to try. I built this in span of 1 week due to other life activities. It is awarded as the <span>BEST PROJECT ONE</span> in the bootcamp. <span>(currently rebuilding)</span></p>
+
+                                </div>
+
+                                <div className="project-tech">
+
+                                    <h3>Tech Stacks: </h3>
+
+                                    <div className="tools">
                                         <span>html</span>
                                         <span>css</span>
                                         <span>javascript</span>
@@ -35,49 +44,84 @@ function Projects() {
                                         <span>canva</span>
                                         <span>git</span>
                                         <span>vercel</span>
-                                        <span>Illustrator</span>
 
                                     </div>
 
                                 </div>
 
+                                <div className="project-btn">
+                                    <a href="" className='btn btn-primary'>view website</a>
+                                    <a href="" className='btn btn-primary'>github repository</a>
+                                </div>
+
                             </div>
 
-                            <div className="project-img col-md-6 demoTwo">
+                            <div className="project-img  demoTwo">
 
                                 <img src={Ehub} alt="" />
-                                <div className="layer">
 
 
-                                    <div className="project-tools">
+                                <div className="project-txt">
 
-                                        <span>html</span>
-                                        <span>css</span>
-                                        <span>javascript</span>
-                                        <span>bootstrap</span>
-                                        <span>figma</span>
-                                        <span>canva</span>
-                                        <span>git</span>
-                                        <span>vercel</span>
-                                        <span>WebApis</span>
-                                        <span>React</span>
-                                        <span>Postman</span>
-                                        <span>Illustrator</span>
-                                        <span>firebase</span>
+                                    <h3>Ehub</h3>
+
+                                    <p>Ehub a viewing books website where a user can search an author, books and genres he/she wants to find. <span>(currently rebuilding)</span></p>
+
+                                </div>
+
+                                <div className="project-tech">
+
+                                    <h3>Tech Stacks:</h3>
+
+                                    <div className="tools">
+
+                                        <div className="project-tools">
+
+                                            <span>html</span>
+                                            <span>css</span>
+                                            <span>javascript</span>
+                                            <span>bootstrap</span>
+                                            <span>figma</span>
+                                            <span>canva</span>
+                                            <span>git</span>
+                                            <span>vercel</span>
+                                            <span>WebApis</span>
+                                            <span>React</span>
+                                            <span>Postman</span>
+                                            <span>Illustrator</span>
+                                            <span>firebase</span>
+
+                                        </div>
 
                                     </div>
 
                                 </div>
+
+                                <div className="project-btn">
+                                    <a href="" className='btn btn-primary'>view website</a>
+                                    <a href="" className='btn btn-primary'>github repository</a>
+                                </div>
+
+
                             </div>
 
-                            <div className="project-img col-md-6 demoThree">
+                            <div className="project-img demoThree">
 
                                 <img src={FuelFlow} alt="" />
-                                <div className="layer">
 
 
-                                    <div className="project-tools">
+                                <div className="project-txt">
 
+                                    <h3>FuelFlow</h3>
+
+                                    <p>FuelFlow is a web application where a user can order a fuel for his vehicle just using his/her device converniently. <span>(currently building)</span></p>
+                                </div>
+
+                                <div className="project-tech">
+
+                                    <h3>Tech Stacks: </h3>
+
+                                    <div className="tools">
                                         <span>html</span>
                                         <span>css</span>
                                         <span>javascript</span>
@@ -97,6 +141,15 @@ function Projects() {
                                     </div>
 
                                 </div>
+
+                                <div className="project-btn">
+                                    <a href="" className='btn btn-primary disabled'>not available for now</a>
+                                    <a href="" className='btn btn-primary'>github repository</a>
+                                </div>
+
+
+
+
                             </div>
                         </div>
                     </div>
